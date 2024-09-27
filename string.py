ceeb8cbe8e1132d3adc1b3016bd1f3a773735804
Maneesha-15 <127533735+Maneesha-15@users.noreply.github.com>
2024-09-27 12:44:05 +0530

    first commit

---
+++ string.py
@@ -0,0 +1,11 @@
+string = 'THIS IS GOOD'
+if string.isupper() == True:
+  print('Does not contain lowercase letter.')
+else:
+  print('Contains lowercase letter.')
+  
+string = 'THIS IS gOOD'
+if string.isupper() == True:
+  print('Does not contain lowercase letter.')
+else:
+  print('Contains lowercase letter.')
