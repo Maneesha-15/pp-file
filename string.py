cf1ac945444d76f812a07b8c5907a9b1d9370ed7
gitu-lab <ganithalakshmig@gmail.com>
2024-09-29 16:04:10 +0530

    Update string.py

--- string.py
+++ string.py
@@ -3,7 +3,6 @@ if string.isupper() == True:
   print('Does not contain lowercase letter.')
 else:
   print('Contains lowercase letter.')
-  
 string = 'THIS IS gOOD'
 if string.isupper() == True:
   print('Does not contain lowercase letter.')
